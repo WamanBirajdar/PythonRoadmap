e6fbab335dfb4752e9e297b732c0fe7ef029808d
WAMAN BIRAJDAR <78862138+WamanBirajdar@users.noreply.github.com>
2022-12-27 15:27:46 +0530

    Create Python_Operators.py

---
+++ Python_Operators.py
@@ -0,0 +1,23 @@
+# Python operators
+1.Python arithmetic operator
+2.Python comparison operator
+3.Python logical operator
+
+
+# PYTHON ARITHMETIC OPERATOR
+--> addition operator
+--> substraction operator
+--> multiplication operator
+--> divison operator
+--> float divion operator
+--> modules operator
+
+# PYTHON COMPARISON OPERATOR
+--> == equal to 
+--> != not equal to 
+--> >  greater than 
+--> <  less than 
+--> >= greater than equal to 
+--> <= less than equal to 
+
+#PYTHON LOGICAL OPERATOR
