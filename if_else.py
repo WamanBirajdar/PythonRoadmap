d7e2b56495fe82358cec2ad33fde282a86f7e029
WAMAN BIRAJDAR <78862138+WamanBirajdar@users.noreply.github.com>
2022-12-28 10:37:36 +0530

    Create if_else.py

---
+++ if_else.py
@@ -0,0 +1,28 @@
+# in python we if else statement for decision making if condition is true then execute if statement code its failse then execute else condition
+
+# if statement
+'''
+if <condition>:
+    exucation statement
+'''
+
+# if...else statement
+'''
+if <condition>:
+    exucation statement here
+else <condition>:
+    execution statement here
+'''
+
+
+#nested if else statement
+if <condition>:
+    if <condition>:
+           if <condition>:
+               execution statement
+            else:
+                execuion statement
+     else:
+         execution statement
+else:
+    execution statement
