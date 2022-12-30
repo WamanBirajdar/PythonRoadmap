9b445ac4a3823a5bb0fcad3ba3c5b1eb33d880c5
WAMAN BIRAJDAR <78862138+WamanBirajdar@users.noreply.github.com>
2022-12-30 13:58:39 +0530

    Create whileLoop.py

---
+++ whileLoop.py
@@ -0,0 +1,10 @@
+#program for while loop
+
+num=1
+while num<=10:
+    print(num)
+    num+=1 #commeted this line it will infinite loop run program infinite stackoverflow
+
+#we use while true in produce for reading topics
+while True:
+    print("Hello") #it goes in infinite loop
