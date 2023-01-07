f8a417a13987e556a2a0322010ece13bc1377454
WAMAN BIRAJDAR <78862138+WamanBirajdar@users.noreply.github.com>
2023-01-07 16:19:37 +0530

    Create ReplaceElementWithGretest.py

---
+++ DSA/Arrays/ReplaceElementWithGretest.py
@@ -0,0 +1,50 @@
+"""
+Given an array of integers, replace every element with the next greatest element (greatest element on the right side) in the array. 
+Since there is no element next to the last element, replace it with -1. 
+For example, if the array is {16, 17, 4, 3, 5, 2}, then it should be modified to {17, 5, 5, 5, 2, -1}. 
+"""
+
+
+
+# Python Program to replace every element with the
+# greatest element on right side
+
+# Function to replace every element with the next greatest
+# element
+def nextGreatest(arr):
+
+	size = len(arr)
+
+	# Initialize the next greatest element
+	max_from_right = arr[size-1]
+
+	# The next greatest element for the rightmost element
+	# is always -1
+	arr[size-1] = -1
+
+	# Replace all other elements with the next greatest
+	for i in range(size-2,-1,-1):
+
+		# Store the current element (needed later for updating
+		# the next greatest element)
+		temp = arr[i]
+
+		# Replace current element with the next greatest
+		arr[i]=max_from_right
+
+		# Update the greatest element, if needed
+		if max_from_right< temp:
+			max_from_right=temp
+
+# Utility function to print an array
+def printArray(arr):
+	for i in range(0,len(arr)):
+		print (arr[i],end=" ")
+
+# Driver function to test above function
+arr = [16, 17, 4, 3, 5, 2]
+nextGreatest(arr)
+print ("Modified array is")
+printArray(arr)
+# This code is contributed by Devesh Agrawal
+
